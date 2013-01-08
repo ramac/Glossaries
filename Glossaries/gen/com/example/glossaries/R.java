2e811a071afc3d5b45f9259c0f38151dcb9f2299
ramac <kittu43@gmail.com>
2013-01-08 12:24:50 +1100

    completed_s1

--- Glossaries/gen/com/example/glossaries/R.java
+++ Glossaries/gen/com/example/glossaries/R.java
@@ -14,7 +14,9 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_dictionary=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int searchbutton1png=0x7f020002;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090000;
